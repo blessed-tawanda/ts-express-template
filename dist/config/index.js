fe7dd92a358161e11925486edd5f646fadfd655c
Blessed T Mahuni <blessedmahuni@gmail.com>
2021-05-28 14:06:54 +0200

    Delete dist directory

--- dist/config/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.port = exports.dbUrl = void 0;
-exports.dbUrl = process.env.DATABASE_URL ? process.env.DATABASE_URL : '';
-exports.port = process.env.PORT ? process.env.PORT : '0000';
